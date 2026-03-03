2b1b1ec2d92c6ff32558b6d529b553c107da7abb
Gabriela Paré <gabriela.pare@rcpo.com.ar>
2026-03-03 20:34:53 -0300

    Fix en page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import TechStack from "@/components/TechStack";
-import ProfessionalFocus from "@/components/ProfessionalFocus";
-import PersonalSection from "@/components/PersonalSection";
-import Contact from "@/components/Contact";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import TechStack from "../components/TechStack";
+import ProfessionalFocus from "../components/ProfessionalFocus";
+import PersonalSection from "../components/PersonalSection";
+import Contact from "../components/Contact";
 
 export default function Home() {
   return (
